17d096bcecd0901c4ff64b361be31c6138388c45
Ankit Lingwal <ankitlingwal9@gmail.com>
2025-02-25 18:13:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://create-card-fe.vercel.app", credentials: true }));
 app.use(express.json());
 
 // Routes
